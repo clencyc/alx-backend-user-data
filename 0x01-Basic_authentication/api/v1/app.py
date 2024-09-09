80a6708ec2e0a24bcb81d7ba40a7fdae31b57101
clencyc <clency2023@gmail.com>
2024-09-09 13:51:43 +0300

    authorization

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -9,10 +9,27 @@ from flask_cors import (CORS, cross_origin)
 import os
 
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
 
+if os.getenv('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+@app.before_request
+def before_request():
+    """ Before request handler
+    """
+    if auth is None:
+        return
+    if not auth.require_auth(request.path, ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']):
+        if not auth.authorization_header(request):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
 
 @app.errorhandler(404)
 def not_found(error) -> str:
