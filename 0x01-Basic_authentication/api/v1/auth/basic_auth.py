6ec73f882c121c266758c8d8e5ccc98939b7f750
clencyc <clency2023@gmail.com>
2024-09-09 16:22:50 +0300

    base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -16,3 +16,15 @@ class BasicAuth(Auth):
         if authorization_header[:6] != 'Basic ':
             return None
         return authorization_header[6:]
+    
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """ returns the decoded value of a Base64 string base64_authorization_header
+        """
+        import base64
+        if base64_authorization_header is None or\
+              type(base64_authorization_header) is not str:
+            return None
+        try:
+            return base64.b64decode(base64_authorization_header).decode('utf-8')
+        except Exception:
+            return None
