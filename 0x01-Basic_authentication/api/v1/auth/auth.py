b87837db918e5fe069ec14ccee110680c54642f2
clencyc <clency2023@gmail.com>
2024-09-09 13:28:18 +0300

    auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" Module of Auth views
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    This is a class to manage the API authentication.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Method that require authentication
+        """
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
+    
+    def authorization_header(self, request=None) -> str:
+        """ Method that return the value of the header request
+        """
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Method that return None
+        """
+        return None
