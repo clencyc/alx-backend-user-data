0aef9d62548613d8e215bd9f554f084b435b78c2
clencyc <clency2023@gmail.com>
2024-09-09 16:55:20 +0300

    User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -41,3 +41,21 @@ class BasicAuth(Auth):
             return (None, None)
         extract = decoded_base64_authorization_header.split(':', 1)
         return (extract[0], extract[1])
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> User:
+        """ returns the User instance based on his email and password
+        """
+        if user_email is None or user_pwd is None or\
+            type(user_email) is not str or type(user_pwd) is not str:
+            return None
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+        if not users or len(users) == 0:
+            return None
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
+    
