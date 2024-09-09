61b595867088e5bd2e01fda2834b8534cbbc8144
clencyc <clency2023@gmail.com>
2024-09-09 17:12:26 +0300

    require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,10 +17,10 @@ class Auth:
             return True
         if path[-1] != '/':
             path += '/'
-        if path in excluded_paths:
-            return False
+        for pattern in excluded_paths:
+            if fnmatch.fnmatch(path, pattern):
+                return False
         return True
-
     def authorization_header(self, request=None) -> str:
         """ Method that return the value of the header request
         """
