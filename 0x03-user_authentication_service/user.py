9d9e9b1084ce0442c553f382c7e163fb742a6f04
clencyc <clency2023@gmail.com>
2024-09-14 08:01:58 +0300

    Sql alchemy

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+A SQLAlchemy model named User for a
+database table named users
+"""
+
+from sqlalchemy import String, Column, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    """
+    User class for the users table
+    """
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
